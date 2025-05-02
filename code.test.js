99655db055b1a9edd18df61501daf60cc10342c4
mjschrader307 <mjschrader307@gmail.com>
2025-05-01 18:32:37 -0600

    Fixing test

--- code.test.js
+++ code.test.js
@@ -6,23 +6,19 @@ const { Graph, augmentingPath } = require('./code.js');
 var graph = {'foo': {'boo': 7},
     'boo': {'foo': 3, 'bar': 2},
     'bar': {'boo': 4}};
-var G = new Graph(graph);
-assert(JSON.stringify(augmentingPath(G, 'foo', 'bar')) == JSON.stringify(['foo', 'boo', 'bar']));
+assert(JSON.stringify(augmentingPath(graph, 'foo', 'bar')) == JSON.stringify(['foo', 'boo', 'bar']));
 
 var graph = {'foo': {'boo': 7, 'd': 4},
     'boo': {'e': 3},
     'd': {'bar': 4},
     'e': {'foo': 1},
     'bar': {'foo': 2}};
-var G = new Graph(graph);
-assert(JSON.stringify(augmentingPath(G, 'foo', 'bar')) == JSON.stringify(['foo', 'd', 'bar']));
+assert(JSON.stringify(augmentingPath(graph, 'foo', 'bar')) == JSON.stringify(['foo', 'd', 'bar']));
 
 var graph = {'foo': {},
     'bar': {'foo': 1}};
-var G = new Graph(graph);
-assert(JSON.stringify(augmentingPath(G, 'foo', 'bar')) == '[]');
+assert(JSON.stringify(augmentingPath(graph, 'foo', 'bar')) == '[]');
 
 var graph = {'foo': {},
     'bar': {'foo': 1}};
-var G = new Graph(graph);
-assert(JSON.stringify(augmentingPath(G, 'foo', 'foo')) == JSON.stringify(['foo']));
+assert(JSON.stringify(augmentingPath(graph, 'foo', 'foo')) == JSON.stringify(['foo']));
