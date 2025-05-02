9fbea5cd62832f97500a8ed8063de239a4aff31f
mjschrader307 <mjschrader307@gmail.com>
2025-05-01 19:47:23 -0600

    Ready for initial review

--- code.js
+++ code.js
@@ -1,31 +1,3 @@
-// function dfs(graph, current_node, target, visited = [], path = []) {
-//   // Mark current node as visited and add it to path
-//   visited.push(current_node);
-//   path.push(current_node);
-
-//   // Exit condition: current and target nodes are the same
-//   if (current_node === target) return [...path];
-
-//   let neighbors = graph[current_node] || [];
-
-//   for (let i = 0; i < neighbors.length; i++) {
-//     let neighbor = neighbors[i];
-
-//     // Only proceed with a neighbor if it hasn't been looked at
-//     if (!visited.includes(neighbor)) {
-//       let result = dfs(graph, neighbor, target, visited, [...path]);
-
-//       // Should only have a positive length if it found something
-//       if (result.length > 0) {
-//         return result;
-//       }
-//     }
-//   }
-
-//   path.pop();
-//   return [];
-// }
-
 class Graph {
   constructor(adjList) {
     this.graph = JSON.parse(JSON.stringify(adjList));
@@ -55,10 +27,6 @@ class Graph {
 
   getNeighbors(u) {
     return this.graph[u];
-  }
-
-  print() {
-    console.log(this.graph);
   }
 }
 
